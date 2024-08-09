b08c5d9f449c2489bee119fd46277d3142054b81
Bruno Mota <bmotadev@gmail.com>
2024-08-09 12:46:17 -0300

    Sistema para Validacao de Processo seletivo - Imprimindo uma lista com candidatos selecionados

--- validacao-processo-seletivo/src/candidatura/ProcessoSeletivo.java
+++ validacao-processo-seletivo/src/candidatura/ProcessoSeletivo.java
@@ -9,6 +9,11 @@ import java.util.concurrent.ThreadLocalRandom;
  *   - Se não, imprimir: AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS
  * 
  *  Solicitado que no sistema seja selecionado apenas no máximo 5 candidatos para entrevista onde o salário pretendido seja menor ou igual ao salário base
+ * 
+ *  Imprimir a lista dos candidatos selecionados para disponibilizar para o RH entrar em contato
+ * 
+ *  O RH deverá realizar uma ligação com no máximo 03 tentativas para cada candidato selecionado, e caso o candidato atenda. Deve se imprimir:
+ *   - "CONSEGUINDO CONTATO COM [CANDIDATO] APÓS [TENTATIVAS]"
  */
 
 public class ProcessoSeletivo {
@@ -16,7 +21,22 @@ public class ProcessoSeletivo {
         // analisarCandidato(1900.0);
         // analisarCandidato(2200.0);
         // analisarCandidato(2000.0);
-        selecaoCandidatos();
+        imprimirSelecionados();
+    }
+
+    static void imprimirSelecionados() {
+        String [] candidatos = { "BRUNO", "JESSICA", "ROSANGELA", "IRENE", "MARIA"};
+        System.out.println("Imprimindo a lista de candidatos, informando o indice do elemento");
+
+        for(int indice = 0; indice < candidatos.length; indice++) {
+            System.out.println("O candidato de numero " + (indice + 1) + " é o " + candidatos[indice]);
+        }
+
+        System.out.println("Forma abreviada de interação ForEach");
+
+        for(String candidato: candidatos) {
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
     }
 
     static void selecaoCandidatos() {
